d2b7847a0d7518d817ab16105bda52a79d4c7023
ChuDatCN <79975646+ChuDatCN@users.noreply.github.com>
2022-08-04 14:45:27 +0700

    testbug

--- src/components/admin.js
+++ src/components/admin.js
@@ -5,43 +5,17 @@ import config from '../config';
 import './login/css/login.css';
 
 function Admin(props) {
-    let users = [];
-    const token = localStorage.getItem('token');
-    const [Users, SetUsers] = useState(users);
+
     const bearerToken = 'Bearer ' + token;
-        fetch(config['server-domain'] + 'users/user-list', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json',
-                'Authorization': bearerToken
-            }
-        }).then(
-            response => response.json()
-        ).then(json => {
-            users = json;
-            SetUsers(json);
-        });
+        fetch(config['https://umbraco-test.chinhquyendientu.vn/umbraco/api/Tintucnoibo/GetChilrenOfMenusFromNode?nodeId=1097']
+        ).then(response){
+        console.log(response);
+        };
     
 
     
     return (
         <div>
-        <table class="table table-striped">
-
-            <tr class="bg-info">
-                <th>Username</th>
-                <th>Email</th>
-                <th>FullName</th>
-            </tr>
-            {Users.map((user, index) => (
-                <tr>
-                    <th className='input-file'>{user.username}</th>
-                    <th className='input-file'>{user.email}</th>
-                    <th className='input-file'>{user.fullname}</th>
-                </tr>
-            ))}
-            </table>
         </div>
     )
     
